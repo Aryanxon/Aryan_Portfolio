a62cc11f67e9afbb4529252b2c17f9bbf59533d6
Aryanxon <168644067+Aryanxon@users.noreply.github.com>
2024-08-11 10:34:52 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,7 +9,7 @@ import SkillsCard from './SkillsCard'
 
 function Skills() {
   return (
-    <div name='skills' className='bg-[#0a192f] text-gray-300 w-full'>
+    <div name='skills' className='bg-[#0a192f] text-gray-300 w-full mb-12'>
         <div className='md:max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-screen'>
             <div >
                 <p className='text-4xl font-bold text-center border-b-4 border-pink-600'>TechStack</p>
